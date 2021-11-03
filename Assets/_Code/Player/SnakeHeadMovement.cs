789c7ee006954b9f604a6fb017000a86359dce94
5Daydreams <nelsonkossuga@gmail.com>
2021-11-03 17:55:18 +0100

    Working on the Snake Movement by rotation Input;

--- Assets/_Code/Player/SnakeHeadMovement.cs
+++ Assets/_Code/Player/SnakeHeadMovement.cs
@@ -6,9 +6,17 @@ public class SnakeHeadMovement : MonoBehaviour
 {
     [SerializeField] private float baseSpeed;
     [SerializeField] private float boostSpeed;
+    [SerializeField] private float turnSpeedH = 90;
+    [SerializeField] private float turnSpeedV = 30;
     [SerializeField] private bool isBoosting;
-    private float currentSpeed = 1.0f;
+    
     private Vector3 direction = new Vector3(0,0,1);
+    private float currentSpeed = 1.0f;
+    
+    private float angleH = 0;
+    private float angleV = 0;
+    Quaternion rotH = Quaternion.identity;
+    Quaternion rotV = Quaternion.identity;
     
     private void Update()
     {
@@ -18,7 +26,6 @@ public class SnakeHeadMovement : MonoBehaviour
     
     void AdjustRotation()
     {
-        this.transform.rotation = Quaternion.AngleAxis(Time.deltaTime, Vector3.up) * this.transform.rotation;
         direction = this.transform.rotation * Vector3.forward;
     }
 
@@ -27,9 +34,20 @@ public class SnakeHeadMovement : MonoBehaviour
         this.transform.position += direction * currentSpeed * Time.deltaTime;
     }
 
-    public void TurnHorizontal(float angle)
+    public void TurnHorizontal(float angleDir)
     {
-        Quaternion rotH = Quaternion.AngleAxis(angle, Vector3.up);
+        angleH += angleDir * turnSpeedH * Time.deltaTime;
+        rotH = Quaternion.AngleAxis(angleH, Vector3.up);
+
+        this.transform.rotation = rotV * rotH;
+    }
+    
+    public void TurnVertical(float angleDir)
+    {
+        angleV += angleDir * turnSpeedV * Time.deltaTime;
+        rotV = Quaternion.AngleAxis(angleV, transform.right);
+
+        this.transform.rotation = rotV * rotH;
     }
 
     public void SetSpeedBoost(bool value)
