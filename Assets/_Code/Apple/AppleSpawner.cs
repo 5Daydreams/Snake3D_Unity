3a521b2a599503c7a8d1d7a70dd5be4956236e42
5Daydreams <nelsonkossuga@gmail.com>
2021-11-10 18:22:12 +0100

    working on apple spawn;

--- Assets/_Code/Apple/AppleSpawner.cs
+++ Assets/_Code/Apple/AppleSpawner.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using _Code.Apple;
@@ -5,9 +6,30 @@ using UnityEngine;
 
 public class AppleSpawner : MonoBehaviour
 {
-    [SerializeField] private AppleCollisionCallback _appleList; 
-    public void SpawnApple()
+    [SerializeField] private List<AppleAndWeight> _appleList;
+    private List<AppleCollisionCallback> _listToChoseFrom;
+
+    public void SpawnRandomApple()
     {
         
+    }
+
+    private void OnValidate()
+    {
+        _listToChoseFrom.Clear();
+        foreach (AppleAndWeight apple in _appleList)
+        {
+            for (int i = 0; i < apple.Repeats; i++)
+            {
+                _listToChoseFrom.Add(apple.AppleReference);
+            }
+        }
+    }
+
+    [Serializable]
+    public class AppleAndWeight
+    {
+        public int Repeats;
+        public AppleCollisionCallback AppleReference;
     }
 }
