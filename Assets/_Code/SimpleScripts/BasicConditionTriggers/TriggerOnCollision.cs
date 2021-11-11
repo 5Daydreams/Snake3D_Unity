468825b60ec124a3c262460ae010d10b7c3b5bca
5Daydreams <nelsonkossuga@gmail.com>
2021-11-11 04:15:43 +0100

    hotfixed an architecture issue with collision triggers;

--- Assets/_Code/SimpleScripts/BasicConditionTriggers/TriggerOnCollision.cs
+++ Assets/_Code/SimpleScripts/BasicConditionTriggers/TriggerOnCollision.cs
@@ -1,5 +1,6 @@
 using _Code.CustomEvents.VoidEvent;
 using UnityEngine;
+using UnityEngine.Events;
 
 namespace _Code.SimpleScripts.BasicConditionTriggers
 {
@@ -8,18 +9,41 @@ namespace _Code.SimpleScripts.BasicConditionTriggers
     {
         [Tooltip("Leave as empty string if no tag is required")]
         [SerializeField] private string _targetTag = "";
-        [SerializeField] private VoidEvent _onTriggerEnterCallback;
+        [SerializeField] private bool _destroyOther;
+        [SerializeField] private bool _destroySelf;
+        [SerializeField] private UnityEvent _onTriggerEnterCallback;
+
         private void OnTriggerEnter(Collider other)
         {
+            if (!other.enabled)
+            {
+                return;
+            }
+
             if (_targetTag == "")
             {
-                _onTriggerEnterCallback?.Raise();
+                ResolveCallbacks(other);
                 return;
             }
 
             if (other.CompareTag(_targetTag))
             {
-                _onTriggerEnterCallback?.Raise();
+                ResolveCallbacks(other);
+            }
+        }
+
+        private void ResolveCallbacks(Collider other)
+        {
+            _onTriggerEnterCallback?.Invoke();
+
+            if (_destroyOther)
+            {
+                Destroy(other.gameObject);
+            }
+            
+            if (_destroySelf)
+            {
+                Destroy(this.gameObject);
             }
         }
     }
