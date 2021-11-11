c5929df29dab6d6b26e5cfe2896f99506ef048c7
5Daydreams <nelsonkossuga@gmail.com>
2021-11-11 03:29:30 +0100

    + Adding the Rock Spawners;

--- Assets/_Code/FlyingRocks/RockSpawner.cs
+++ Assets/_Code/FlyingRocks/RockSpawner.cs
@@ -1,16 +1,22 @@
+using System;
 using _Code.Geometry;
 using UnityEngine;
 
 namespace _Code.FlyingRocks
 {
+    [RequireComponent(typeof(BoundingBox))]
     public class RockSpawner : MonoBehaviour
     {
         [SerializeField] private bool _enableGizmo;
         [SerializeField] private Vector3 _centerPos;
-        [SerializeField] private Vector3 _size;
         [SerializeField] private Vector3 _movementDirection;
-        [SerializeField] private BoundingBox _boundingBox;
         [SerializeField] private StraightLineMovement _rockPrefab;
+        private BoundingBox _boundingBox;
+
+        private void Awake()
+        {
+            _boundingBox = this.GetComponent<BoundingBox>();
+        }
 
         private void OnDrawGizmos()
         {
@@ -18,18 +24,19 @@ namespace _Code.FlyingRocks
             {
                 return;
             }
-
-            Gizmos.DrawWireCube(this.transform.position + _centerPos, _size);
+            
+            Gizmos.color = Color.red;
+            Gizmos.DrawRay(transform.position, _movementDirection * 200.0f);
+            Gizmos.color = Color.white;
         }
 
         public void SpawnRockAtRandomPosition()
         {
             Vector3 spawnPosition = _boundingBox.GetRandomPositionInside();
-            
-            StraightLineMovement spawnedRock = Instantiate(_rockPrefab,spawnPosition,Quaternion.identity);
+
+            StraightLineMovement spawnedRock = Instantiate(_rockPrefab, spawnPosition, Quaternion.identity);
 
             spawnedRock.Direction = _movementDirection.normalized;
         }
-
     }
 }
